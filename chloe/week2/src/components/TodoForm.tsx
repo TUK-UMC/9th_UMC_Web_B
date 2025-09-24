5785689877a0dc9e2f8a4e59a4de24b93195f1c8
차윤서 <chays1031@tukorea.ac.kr>
2025-09-24 17:49:10 +0900

    FEAT: 2주차 과제1 제출

---
+++ chloe/week2/src/components/TodoForm.tsx
@@ -0,0 +1,35 @@
+import {useState, FormEvent} from "react";
+import { useTodo } from "../context/TodoContext";
+
+const TodoForm = () : React.ReactElement => {
+    const [input, setInput] = useState<string>('');
+    const { addTodo } = useTodo();
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) : void =>{
+        e.preventDefault();
+        const text = input.trim();
+    
+        if(text){
+            addTodo(text);
+            setInput('');
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} 
+        className='todo-container__form'>
+            <input 
+                value={input}
+                onChange={(e):void => setInput(e.target.value)}
+                className='todo-container__input' 
+                type="text" 
+                placeholder='할 일을 입력'
+                required
+            />
+            <button className='todo-container__button' type='submit'>
+                할 일 추가
+            </button>
+        </form>
+    );
+};
+
+export default TodoForm;
