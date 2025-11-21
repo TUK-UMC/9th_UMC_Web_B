75e4d71b6fc8ec10120bd24916e880c7163ece72
유정 <alth1740@gamil.com>
2025-11-21 12:52:46 +0900

    Feat: 9주차 실습1

--- 유뎅/week9/src/App.tsx
+++ 유뎅/week9/src/App.tsx
@@ -1,16 +1,18 @@
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 import "./App.css";
-import { CartList } from "./components/CartList";
-import { Navbar } from "./components/Navbar";
-import { store } from "./store/store";
-import { PriceBox } from "./components/PriceBox";
+import UseReducerCompany from "./useReducer/UseReducerCompany";
+// import { CartList } from "./components/CartList";
+// import { Navbar } from "./components/Navbar";
+// import { store } from "./store/store";
+// import { PriceBox } from "./components/PriceBox";
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <Navbar />
-      <CartList />
-      <PriceBox />
-    </Provider>
+    // <Provider store={store}>
+    //   <Navbar />
+    //   <CartList />
+    //   <PriceBox />
+    // </Provider>
+    <UseReducerCompany />
   );
 }
