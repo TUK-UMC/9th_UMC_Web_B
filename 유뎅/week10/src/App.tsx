90f7c74e211510aea27fdf9868459ad9f288e8a3
유정 <choiyu0wo0d@tukorea.ac.kr>
2025-11-25 23:10:42 +0900

    Feat: 10주차 실습1

---
+++ 유뎅/week10/src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+import UseCallbackPage from "./useCallback-memo/UseCallbackPage";
+
+export default function App() {
+  return (
+    <main className="flex flex-col justify-center items-center h-dvh">
+      <UseCallbackPage />
+    </main>
+  );
+}
