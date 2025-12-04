39feb3d9bd4f7532ea2a62963da4e88627a140ab
유정 <choiyu0wo0d@tukorea.ac.kr>
2025-12-04 22:53:26 +0900

    Feat: 10주차 미션2

--- 유뎅/Optimization-Lecture/src/App.tsx
+++ 유뎅/Optimization-Lecture/src/App.tsx
@@ -1,8 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import MovieDetailPage from "./pages/MovieDetailPage";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/:id", element: <MovieDetailPage /> },
+]);
 
 function App() {
-  return <HomePage />;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
